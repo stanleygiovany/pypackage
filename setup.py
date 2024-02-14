45de66f5ab22513cf6da71eb16fa609847652fa8
stanleygiovany <stanley.giovany@gmail.com>
2024-02-14 10:57:58 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 FILE = Path(__file__).resolve()
+REQUIREMENTS_CPU = [
+    "pytest"
+]
+REQUIREMENTS_GPU = [
+    "flask"
+]
 REQUIREMENTS = [
     "numpy"
 ]
@@ -12,5 +18,9 @@ setup(
     name="pypackage",
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=REQUIREMENTS
+    install_requires=REQUIREMENTS,
+    extras_require={
+        "cpu": REQUIREMENTS_CPU,
+        "gpu": REQUIREMENTS_GPU
+    }
 )
